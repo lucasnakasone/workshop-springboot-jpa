4d1c2a3145498534789af696f96603845b194c46
Lucas Nakasone <lucasnakasone@hotmail.com>
2023-01-18 01:27:29 -0300

    Project created

---
+++ src/main/java/com/vysesocial/stscourse/StsCourseApplication.java
@@ -0,0 +1,13 @@
+package com.vysesocial.stscourse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StsCourseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StsCourseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vysesocial/stscourse/StsCourseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vysesocial.stscourse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StsCourseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
