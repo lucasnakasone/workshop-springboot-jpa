30877ea7e8eacbd8199f6b5c06c5c440aac6cd9c
Lucas Nakasone <lucasnakasone@hotmail.com>
2023-01-23 22:29:51 -0300

    Implemented - Exception handling @ /user findById

--- src/main/java/com/vysesocial/stscourse/services/UserService.java
+++ src/main/java/com/vysesocial/stscourse/services/UserService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 
 import com.vysesocial.stscourse.entities.User;
 import com.vysesocial.stscourse.repositories.UserRepository;
+import com.vysesocial.stscourse.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class UserService {
@@ -21,7 +22,7 @@ public class UserService {
 	
 	public User findById(Long id) {
 		Optional<User> obj = repository.findById(id);
-		return obj.get();
+		return obj.orElseThrow(() -> new ResourceNotFoundException(id));
 	}
 	
 	public User insert(User user) {
